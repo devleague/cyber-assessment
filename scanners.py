ec2011f5ba0a0a120bd5ff66a4a0825b367a5fce
Chaz Leong <cleong14@gmail.com>
2018-09-24 23:54:09 -1000

    Successfully writing scan count to scanners_found.txt

--- scanners.py
+++ scanners.py
@@ -5,11 +5,20 @@ with open('./devleague_discovery/ssh.log.txt', 'r') as file:
   all_lines = file.readlines()
   for i, lines in enumerate(all_lines):
     scan_count = i
+    each_line = all_lines[i]
 
+    # found_scans = open('scanners_found.txt', 'a+')
+    # found_scans.write(each_line)
+
+    print('EACH LINE', each_line)
+
+  found_scan_count = open('scanners_found.txt', 'a+')
+  found_scan_count.write('Scan Count\n')
+  found_scan_count.write(str(scan_count))
+  found_scan_count.close()
   print('SCAN COUNT', scan_count)
   
   # print(len(all_lines))
-    # print('EACH LINE', all_lines[i])
   # print(all_lines, 'ALL LINES')
 
   # print(len(data))
