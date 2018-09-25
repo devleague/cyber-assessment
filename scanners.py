64cf7be727eb30f2a5c867687386369965f2cfa2
Chaz Leong <cleong14@gmail.com>
2018-09-24 22:11:45 -1000

    Found total scan count

--- scanners.py
+++ scanners.py
@@ -1,5 +1,28 @@
 # import ssh.log
-ssh_log = open('./devleague_discovery/ssh.log.txt', 'r')
+with open('./devleague_discovery/ssh.log.txt', 'r') as file:
+
+  # all lines
+  all_lines = file.readlines()
+  for i, lines in enumerate(all_lines):
+    scan_count = i
+
+  print('SCAN COUNT', scan_count)
+  
+  # print(len(all_lines))
+    # print('EACH LINE', all_lines[i])
+  # print(all_lines, 'ALL LINES')
+
+  # print(len(data))
+  
+  # for i, scan in enumerate(data):
+  #   print(data[i])
+
+  # with open('scanners_found.txt', 'r+') as f:
+  #   scanner_data = f.read()
+  #   print()
+  #   for line in scanner_data:
+  #     print('LINE', line)
+
 
 # print ssh_log
-print(ssh_log.read())
+# print(data)
