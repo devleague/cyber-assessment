355f2bca31b597892d6169b49d039cec86c74901
Chaz Leong <cleong14@gmail.com>
2018-09-26 01:15:09 -1000

    Found hashed ip

--- scanners.py
+++ scanners.py
@@ -9,67 +9,42 @@ with open('./devleague_discovery/ssh.log.txt', 'r') as file:
 
   # scan count
   scan_count = len(all_lines)
+
+  # create scanners_found.txt if doesnt exist in append
   scanners_found = open('scanners_found.txt', 'a+')
+
+  # write scan attempts
   scanners_found.write('[scan attempts] ')
   scanners_found.write(str(scan_count) + '\n\n')
 
-  # scan origin hosts
+  # scan origin hosts - header
   scanners_found.write('[scan origin hosts]\n')
-  # scanners_found.write('\txxx.xxx.x.xx\n\n')
 
+  # write all origin ip
   for line in all_lines:
-    # print(line)
-
     # split line values into list
     line_col_value = line.split()
-    print('SCAN LIST', line_col_value)
+    # print('SCAN LIST', line_col_value)
 
-    # origin ip:port
-    
+    # origin ip
     scan_origin = line_col_value[2]
-    print('ORIGIN', scan_origin)
-    scanners_found.write('\t' + scan_origin + '\n\n')
+    # print('ORIGIN', scan_origin)
+    scanners_found.write('\t' + scan_origin + '\n')
 
+    # questionable ips
+    # print('LENGTH', len(scan_origin))
+    if(len(scan_origin) > 15):
+      print('WHO DIS?!', scan_origin)
 
-    # destination ip:port
+  # scan destination hosts - header
+  scanners_found.write('[scan destination hosts]\n')  
+
+  # write all destination ips
+  for line in all_lines:
+
+    # destination ip
     scan_destination = line_col_value[4]
-    print('DESTINATION', scan_destination + '\n')
+    # print('DESTINATION', scan_destination + '\n')
+    scanners_found.write('\t' + scan_destination + '\n')
 
-    # for i, char in enumerate(line):
-    #   print('INDEX', i)
-    #   print('EACH CHAR', char)
-
-  # scan destination hosts
-  # scanners_found.write('[scan destination hosts]\n')
-  # scanners_found.write('\txxx.xxx.x.xx')
   scanners_found.close()
-
-  # for i, lines in enumerate(all_lines):
-  #   scan_number = i
-  #   each_line = all_lines[i]
-
-    # print('EACH LINE', each_line)
-  
-    # found_scans = open('scanners_found.txt', 'a+')
-    # found_scans.write(each_line)
-  
-
-
-
-  # print(len(all_lines))
-  # print(all_lines, 'ALL LINES')
-
-  # print(len(data))
-  
-  # for i, scan in enumerate(data):
-  #   print(data[i])
-
-  # with open('scanners_found.txt', 'r+') as f:
-  #   scanner_data = f.read()
-  #   print()
-  #   for line in scanner_data:
-  #     print('LINE', line)
-
-
-# print ssh_log
-# print(data)
