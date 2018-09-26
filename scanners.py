a8a4647e3098f2683315d60fa9315051fbc8ba69
Chaz Leong <cleong14@gmail.com>
2018-09-26 00:53:36 -1000

    Changing analysis approach to a more raw method

--- scanners.py
+++ scanners.py
@@ -1,6 +1,7 @@
-# import ssh.log
 # origin (new port each connection) = user machine that attempted scan
 # destination (has reserved port) = machine that user attempted to scan
+
+# import ssh.log
 with open('./devleague_discovery/ssh.log.txt', 'r') as file:
 
   # all lines
@@ -14,18 +15,40 @@ with open('./devleague_discovery/ssh.log.txt', 'r') as file:
 
   # scan origin hosts
   scanners_found.write('[scan origin hosts]\n')
-  scanners_found.write('\txxx.xxx.x.xx\n\n')
+  # scanners_found.write('\txxx.xxx.x.xx\n\n')
+
+  for line in all_lines:
+    # print(line)
+
+    # split line values into list
+    line_col_value = line.split()
+    print('SCAN LIST', line_col_value)
+
+    # origin ip:port
+    
+    scan_origin = line_col_value[2]
+    print('ORIGIN', scan_origin)
+    scanners_found.write('\t' + scan_origin + '\n\n')
+
+
+    # destination ip:port
+    scan_destination = line_col_value[4]
+    print('DESTINATION', scan_destination + '\n')
+
+    # for i, char in enumerate(line):
+    #   print('INDEX', i)
+    #   print('EACH CHAR', char)
 
   # scan destination hosts
-  scanners_found.write('[scan destination hosts]\n')
-  scanners_found.write('\txxx.xxx.x.xx')
+  # scanners_found.write('[scan destination hosts]\n')
+  # scanners_found.write('\txxx.xxx.x.xx')
   scanners_found.close()
 
-  for i, lines in enumerate(all_lines):
-    scan_number = i
-    each_line = all_lines[i]
+  # for i, lines in enumerate(all_lines):
+  #   scan_number = i
+  #   each_line = all_lines[i]
 
-    print('EACH LINE', each_line)
+    # print('EACH LINE', each_line)
   
     # found_scans = open('scanners_found.txt', 'a+')
     # found_scans.write(each_line)
